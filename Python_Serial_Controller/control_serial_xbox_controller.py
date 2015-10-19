2785ec725579baf8faf96e5a508f3bfb83d033d1
Logan Rickert <logan@rickertcorp.com>
2015-10-19 18:42:44 +0000

    Hopefully works as intended

--- Python_Serial_Controller/control_serial_xbox_controller.py
+++ Python_Serial_Controller/control_serial_xbox_controller.py
@@ -2,7 +2,6 @@ import pygame
 import serial
 from pygame.locals import *
 
-
 #########
 
 ## Ignore all the pygame stuff.
@@ -19,6 +18,13 @@ WIN_HEIGHT = 480
 arduino = serial.Serial('/dev/ttyACM0', 9600)
 
 def main():
+  # Wait until the last serial write has finished
+  serialWriteDone = True
+  # What the new value to write is.
+  serialOutputToWrite = 0
+  # What the last value wrote was.
+  lastSerialOutputToWrite = serialOutputToWrite
+
   # Ignore all this stuff
   pygame.init()
   screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
@@ -36,9 +42,25 @@ def main():
   
   while True:
     pygame.time.wait(500)
-    print "Starting new cycle"
-    hasJoyUpdated = False    
 
+    # - print "Checking", serialOutputToWrite, lastSerialOutputToWrite
+
+    if serialWriteDone and serialOutputToWrite != lastSerialOutputToWrite:
+      serialWriteDone = False
+      arduino.write(serialOutputToWrite + '\n')
+
+      while arduino.inWaiting() > 0:
+        print arduino.read(arduino.inWaiting())
+
+      lastSerialOutputToWrite = serialOutputToWrite
+      serialWriteDone = True
+
+    # Get the current value of the joystick.
+    axis1Pos = joysticks[0].get_axis(1)
+    value = str(int(axis1Pos * 10) * -1 + 11)
+    serialOutputToWrite = value
+
+    # Ignore this stuff
     for event in pygame.event.get():
       if event.type == QUIT:
         print "Exiting the window..."
@@ -46,22 +68,21 @@ def main():
       elif event.type == KEYDOWN:
         print "Key down:", event.key
       elif event.type == JOYAXISMOTION:
+        pass
         # 1 = Left stick, up to down, -1 to 1
         # 2 = Left stick, left to right, -1 to 1
         # 3 = Right stick, left to right, -1 to 1
         # 4 = Right stick, up to down, -1 to 1
-        if event.axis == 1 and not hasJoyUpdated:
-          hasJoyUpdated = True
-          print "joystick", joysticks[event.joy].get_name()
+        # - if event.axis == 1:
+          # - print "joystick", joysticks[event.joy].get_name()
           
           # Invert axis, change range from -1 to 1 to -10 to 10
           # Also change from float to int to a string.
-          value = str(int(event.value * 10) * -1)
-          print "axis", event.axis, "value", value
+          # - value = str(int(event.value * 10) * -1 + 11)
+          # - print "axis", event.axis, "value", value
           
           # Send it to the Arduino
-          arduino.write(value)
-          print arduino.readline()
+          # - serialOutputToWrite = value
 
 if __name__ == "__main__":
   main()
