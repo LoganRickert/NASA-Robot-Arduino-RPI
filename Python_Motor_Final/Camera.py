a6a13a3c8cfc0e8dbffe9b686724a1ecb010ebd5
Logan Rickert <logan@rickertcorp.com>
2016-03-26 15:55:08 -0400

    Indented wrong again

--- Python_Motor_Final/Camera.py
+++ Python_Motor_Final/Camera.py
@@ -63,51 +63,51 @@ class Camera:
         self.display.blit(self.snapshot, (0,0))
         pygame.display.flip()
 
-        def compress(self, pixels):
-            change = []
+    def compress(self, pixels):
+        change = []
 
-            delta = pixels[0] + 31
-            change.append(1)
-            change.append(delta)
-            largest = 1
-            largestD = delta
-            lowest = delta
+        delta = pixels[0] + 31
+        change.append(1)
+        change.append(delta)
+        largest = 1
+        largestD = delta
+        lowest = delta
 
-            for item in pixels[1:]:
-                now = pixels[-1] - item + 31
+        for item in pixels[1:]:
+            now = pixels[-1] - item + 31
 
-                if now == change[-1]:
-                    if change[-2] > 5:
-                        tempb = change[-1]
-                        change.append(1)
-                        change.append(tempb)
-                    else:
-                        change[-2] += 1
-
-                    if change[-2] > largest: largest = change[-2]
-                else:
+            if now == change[-1]:
+                if change[-2] > 5:
+                    tempb = change[-1]
                     change.append(1)
-                    change.append(now)
+                    change.append(tempb)
+                else:
+                    change[-2] += 1
 
-                    delta = now
-                    if delta > largestD: largestD = delta
-                    if delta < lowest: lowest = delta
+                if change[-2] > largest: largest = change[-2]
+            else:
+                change.append(1)
+                change.append(now)
 
-            new_list = []
+                delta = now
+                if delta > largestD: largestD = delta
+                if delta < lowest: lowest = delta
 
-            numberRange = largestD - lowest
+        new_list = []
 
-            print 'largest:', largest
-            print 'largestD:', largestD
-            print 'lowest:', lowest
-            print 'delta:', numberRange
-            print 'Change length:', len(change)
-            compresseda = bz2.compress(''.join(str(pixels)), 9)
-            compressedb = bz2.compress(''.join(str(change)), 9)
-            print 'new lista length:', len(compresseda) * 8
-            print 'new listb length:', len(compressedb) * 8
+        numberRange = largestD - lowest
 
-            return len(change)
+        print 'largest:', largest
+        print 'largestD:', largestD
+        print 'lowest:', lowest
+        print 'delta:', numberRange
+        print 'Change length:', len(change)
+        compresseda = bz2.compress(''.join(str(pixels)), 9)
+        compressedb = bz2.compress(''.join(str(change)), 9)
+        print 'new lista length:', len(compresseda) * 8
+        print 'new listb length:', len(compressedb) * 8
+
+        return len(change)
 
 def main():
     camera = Camera()
