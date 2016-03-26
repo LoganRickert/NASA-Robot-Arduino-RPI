848d8e4c8e3ea30cc004ede2d0542048e5ab78b6
Logan Rickert <logan@rickertcorp.com>
2016-03-26 17:32:25 -0400

    Working on it

--- Python_Motor_Final/Camera.py
+++ Python_Motor_Final/Camera.py
@@ -54,7 +54,6 @@ class Camera:
         tempSurface = pygame.surface.Surface(self.size, 0, self.display)
         tempSurface = self.camera.get_image(tempSurface)
 
-        # pxarrayB = pygame.PixelArray(self.snapshot)[0::3, 0::3]
 
         self.camera.stop()
 
@@ -62,32 +61,30 @@ class Camera:
 
         pixels = []
 
-        pxarrayA = pygame.PixelArray(tempSurface)[0::3, 0::3]
+        pxarrayA = pygame.PixelArray(tempSurface)[0::4, 0::4]
+        pxarrayB = pygame.PixelArray(self.snapshot)[0::4, 0::4]
 
         lenx = len(pxarrayA)
         leny = len(pxarrayA[0])
 
-        _calc_pixel_color(pxarrayA)
+        # _calc_pixel_color(pxarrayA)
 
+        for x in range(0, lenx):
+            for y in range(0, leny):
+                col = pxarrayA[x, y]
+                new_val = ((((((col >> 16) & 0xff)*76) + (((col >> 8) & 0xff)*150) + \
+                    ((col & 0xff)*29)) >> 8))
 
-        # for x in range(0, lenx):
-        #     for y in range(0, leny):
-        #         col = pxarrayA[x, y]
-        #         new_val = ((((((col >> 16) & 0xff)*76) + (((col >> 8) & 0xff)*150) + \
-        #             ((col & 0xff)*29)) >> 8))
-
-        #         div = 8
-        #         color = new_val / div
-        #         pixels.append(color)
-        #         pxarrayB[x, y] = (color * div, color * div, color * div)
+                div = 8
+                color = new_val / div
+                pixels.append(color)
+                pxarrayB[x, y] = (color * div, color * div, color * div)
 
         del pxarrayA
-        
-        self.snapshot = pygame.surface.Surface(self.size, 0, tempSurface)
+        del pxarrayB
 
         print "Took:", (time.time() - time_start)
 
-        # del pxarrayB
         # print "Went through:", self.compress(pixels)
 
         self.display.blit(self.snapshot, (0,0))
