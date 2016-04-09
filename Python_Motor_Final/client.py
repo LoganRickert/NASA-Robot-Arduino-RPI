cda9b08681dbf8879186a5214db6bba9795ce16b
Logan Rickert <logan@rickertcorp.com>
2016-04-09 15:00:02 -0400

    Changed stuff

--- Python_Motor_Final/client.py
+++ Python_Motor_Final/client.py
@@ -13,9 +13,14 @@ import bz2
 import binascii
 import ast
 
-size = (160, 120)
+size = (640, 360)
+
+x_scale = 3
+y_scale = 3
+
 pygame.init()
 display = pygame.display.set_mode(size)
+snapshot = pygame.surface.Surface(size, 0, display)
 
 def main():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -27,6 +32,51 @@ def main():
     should_continue = True
 
     while should_continue:
+        current = time.time()
+        client.send('R0\n')
+        temp = ''.join(recvall(client))
+        print "Time:                            ", time.time() - current
+        current = time.time()
+        image = bz2.decompress(binascii.unhexlify(temp))
+        print "sent:", len(temp)
+        print "comp:", len(binascii.unhexlify(temp))
+        print "pixels:", len(image)
+        image = ast.literal_eval(image)
+
+        tempSurface = pygame.surface.Surface(size, 0, display)
+        pxarrayA = pygame.PixelArray(snapshot)
+
+        lenx = len(pxarrayA)
+        leny = len(pxarrayA[0])
+
+        print 'lenx:', lenx
+        print 'leny:', leny
+
+        color_end = []
+
+        for x in range(0, lenx):
+            current_color = []
+            for y in range(0, leny):
+                money_shot = (leny / y_scale) * (x / x_scale) + (y / y_scale)
+                if (money_shot) < len(image):
+                    color = image[money_shot]
+                    div = 8
+                    pxarrayA[x, y] = (color * div, color * div, color * div)
+
+        del pxarrayA
+
+        print "Flipping surface"
+        display.fill((255, 255, 255))
+        print "Blitzing"
+        display.blit(snapshot, (0,0))
+        print "Flipping"
+        pygame.display.flip()
+        print "Waiting"
+        # pygame.event.wait()
+        print "Done                     +", time.time() - current
+        print "Done."
+
+    while should_continue:
         console = raw_input('> ')
         client.send(console + '\n')
 
@@ -34,8 +84,9 @@ def main():
             print 'Got back:', recvall(client)
 
         if console[0] == 'R':
-            
+            current = time.time()
             temp = ''.join(recvall(client))
+            print time.time() - current
             image = bz2.decompress(binascii.unhexlify(temp))
             print "sent:", len(temp)
             print "comp:", len(binascii.unhexlify(temp))
@@ -43,22 +94,30 @@ def main():
             image = ast.literal_eval(image)
 
             tempSurface = pygame.surface.Surface(size, 0, display)
-            pxarrayA = pygame.PixelArray(tempSurface)
+            pxarrayA = pygame.PixelArray(snapshot)
 
             lenx = len(pxarrayA)
             leny = len(pxarrayA[0])
 
+            print 'lenx:', lenx
+            print 'leny:', leny
+
+            color_end = []
+
             for x in range(0, lenx):
+                current_color = []
                 for y in range(0, leny):
-                    color = image[x  * (y + 1)]
-                    div = 8
-                    pxarrayA[x, y] = (color * div, color * div, color * div)
+                    money_shot = (leny / y_scale) * (x / x_scale) + (y / y_scale)
+                    if (money_shot) < len(image):
+                        color = image[money_shot]
+                        div = 8
+                        pxarrayA[x, y] = (color * div, color * div, color * div)
 
             del pxarrayA
 
             print "Flipping surface"
-            display.fill((0,0,0))
-            display.blit(tempSurface, (0,0))
+            display.fill((255, 255, 255))
+            display.blit(snapshot, (0,0))
             pygame.display.flip()
             pygame.event.wait()
 
@@ -78,7 +137,7 @@ def recvall(client_socket):
     data = data[1:] # We need the z so we can check data[-1]
                     # It will give out of bounce error otherwise.
 
-    print 'Recving:', data.split('\n')[:-1]
+    # print 'Recving:', data.split('\n')[:-1]
 
     return data.split('\n')[:-1]
 
