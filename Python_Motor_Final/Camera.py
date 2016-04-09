e080e92b08ecbf1b09c3c8d8ebec0e617d686c5d
Logan Rickert <logan@rickertcorp.com>
2016-04-09 15:05:33 -0400

    asdf

--- Python_Motor_Final/Camera.py
+++ Python_Motor_Final/Camera.py
@@ -15,49 +15,48 @@ class Camera:
 
     #size of the screen to display
     def __init__(self):
-    #640 by 360 scree size
+        #640 by 360 scree size
         self.size = (640, 360)
 
-    # initialize pygame
+        # initialize pygame
         pygame.init()
 
-    # initial pygame camera
+        # initial pygame camera
         pygame.camera.init()
 
-    # dispalys screen
+        # dispalys screen
         # create a display surface. standard pygame stuff
         self.display = pygame.display.set_mode(self.size, 0)
         
-    # list of all cameras --- location of cameras
+        # list of all cameras --- location of cameras
         # this is the same as what we saw before
         self.clist = pygame.camera.list_cameras()
        
-     # print "Camera list:", self.clist
+        # print "Camera list:", self.clist
 
-    # define camera array --- object of camera
+        # define camera array --- object of camera
         self.cameras = []
 
-    # define camera array --- images of camera
+        # define camera array --- images of camera
         self.current_images = []
 
-    # if no cameras are connected
+        # if no cameras are connected
         if not self.clist:
-
-        # 0 cameras
+            # 0 cameras
             self.total_cameras = 0
             print "NO CAMERAS WERE DETECTED!"
        
-     else:
-            # for each camera
+    else:
+        # for each camera
         for camera in self.clist:
-        # append camera objects to camera list
-                self.cameras.append(pygame.camera.Camera(camera, self.size))
-                # append immage and when it was taken
+            # append camera objects to camera list
+            self.cameras.append(pygame.camera.Camera(camera, self.size))
+            # append immage and when it was taken
             self.current_images.append(["", time.time()])
 
             # create a surface to capture to.  for performance purposes
             # bit depth is the same as that of the display surface.
-        # create the surface once, rather than each time we take an image
+            # create the surface once, rather than each time we take an image
             self.snapshot = pygame.surface.Surface(self.size, 0, self.display)
 
     
@@ -84,7 +83,7 @@ class Camera:
         # if time.time() - self.current_images[camera_number][1] < 1:
             # return self.current_images[camera_number][0]
 
-    # start camera
+        # start camera
         self.cameras[camera_number].start()
         
         # makes temp surface 
@@ -93,39 +92,39 @@ class Camera:
         # save camera immage (full color) to temp surfcae 
         tempSurface = self.cameras[camera_number].get_image(tempSurface)
 
-    # stope camera
+        # stope camera
         self.cameras[camera_number].stop()
 
-    # iterations it want through
+        # iterations it want through
         wentThrough = 0
 
-    # define pixels array
+        # define pixels array
         pixels = []
 
-    # make pixel array of the temp surface that has picture (full color)
+        # make pixel array of the temp surface that has picture (full color)
         pxarrayA = pygame.PixelArray(tempSurface)#[0::4, 0::3]
 
-    # make a pixel of array of what will be the manipulated  pixels
+        # make a pixel of array of what will be the manipulated  pixels
         pxarrayB = pygame.PixelArray(self.snapshot)#[0::4, 0::3]
 
-    # makes a single array from the array of array's that pygame
-    # natively returns
+        # makes a single array from the array of array's that pygame
+        # natively returns
         # temp1 = list(itertools.chain(*pxarrayB[0::3, 0::3]))
         
-    # creates applys the magic function to the single arrya made form 
-    # the pxarrayB 
-    #temp2 = map(self.color_convert, temp1)
+        # creates applys the magic function to the single arrya made form 
+        # the pxarrayB 
+        #temp2 = map(self.color_convert, temp1)
 
-    # length of array of arrays
+        # length of array of arrays
         lenx = len(pxarrayA)
 
-    # length of an array inside the array
+        # length of an array inside the array
         leny = len(pxarrayA[0])
 
         # _calc_pixel_color(pxarrayA)
 
-    # for each array insing the array conatining arrays
-    # to the length of the array containing arrays by the scale (current 3)
+        # for each array insing the array conatining arrays
+        # to the length of the array containing arrays by the scale (current 3)
         for x in range(0, lenx, x_scale):
             current_color = [] #<-- ignor for now
     
@@ -148,7 +147,7 @@ class Camera:
         # copys the new color part onto this display
                 pxarrayB[x, y] = (color * div, color * div, color * div)
 
-    # delete
+        # delete
         del pxarrayA
         del pxarrayB
 
@@ -158,18 +157,18 @@ class Camera:
         #print 'one', sum(temp2)
         #print 'two', sum(pixels)
 
-    # compresses the pixel array
+        # compresses the pixel array
         self.current_images[camera_number] = [self._compress(pixels), time.time()]
         
-    print "total Took:", (time.time() - time_start)
+        print "total Took:", (time.time() - time_start)
 
-    # dispalys the snap shot to the screen (hardware function)
+        # dispalys the snap shot to the screen (hardware function)
         self.display.blit(self.snapshot, (0,0))
 
-    # flip the image (swtich buffer (old image to new imae))
+        # flip the image (swtich buffer (old image to new imae))
         pygame.display.flip()
 
-    # retursn the current compressed image list
+        # retursn the current compressed image list
         return self.current_images[camera_number][0]
 
     # compresses the array
